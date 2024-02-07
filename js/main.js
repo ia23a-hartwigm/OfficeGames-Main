b598eaabc1b64e53a8b86f5b8732a793875506be
Lian Keller <lian.keller@edu.tbz.ch>
2024-02-07 09:09:40 +0100

    about us fertiggestellt

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let swiperBaskets = new Swiper('.home__swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+  })
